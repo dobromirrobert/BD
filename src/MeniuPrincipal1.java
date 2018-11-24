2c8672c433610323bdafd8920428e4c30f3f1d05
manuelagabriela <45312440+manuelagabriela@users.noreply.github.com>
2018-11-24 19:32:02 +0200

    update 1

--- src/MeniuPrincipal1.java
+++ src/MeniuPrincipal1.java
@@ -6,8 +6,24 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
+import net.proteanit.sql.DbUtils;
+
 import javax.swing.JTabbedPane;
 import javax.swing.JButton;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.event.ActionListener;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class MeniuPrincipal1 {
 
@@ -45,6 +61,7 @@ public class MeniuPrincipal1 {
 		frame.setBounds(100, 100, 904, 754);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		BD db = new BD("127.0.0.1", "3306", "policlinica", "root", "silver250");
 		
 		JLabel lblMeniuOperatiiFinanciar = new JLabel("Meniu operatii financiar contabile");
 		lblMeniuOperatiiFinanciar.setFont(new Font("Times New Roman", Font.PLAIN, 43));
@@ -52,11 +69,24 @@ public class MeniuPrincipal1 {
 		frame.getContentPane().add(lblMeniuOperatiiFinanciar);
 		
 		JButton btnProfitPoliclinici = new JButton("Profit policlinici");
-		btnProfitPoliclinici.setBounds(419, 150, 101, 23);
+		btnProfitPoliclinici.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					BD db = new BD("127.0.0.1", "3306", "policlinica", "root", "silver250");
+					PreparedStatement stmt = db.getDbHandle().prepareStatement("SELECT id, denumire, Venituri FROM policlinica;");
+					ResultSet rst = stmt.executeQuery();
+					
+					table.setModel(DbUtils.resultSetToTableModel(rst));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		btnProfitPoliclinici.setBounds(419, 150, 173, 23);
 		frame.getContentPane().add(btnProfitPoliclinici);
 		
 		JButton btnNewButton = new JButton("Profit medici");
-		btnNewButton.setBounds(641, 150, 101, 23);
+		btnNewButton.setBounds(641, 150, 173, 23);
 		frame.getContentPane().add(btnNewButton);
 		
 		table = new JTable();
