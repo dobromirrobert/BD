72b80f4a4144eb8d45312ecbba0467eb481c4765
manuelagabriela <45312440+manuelagabriela@users.noreply.github.com>
2018-11-24 19:03:04 +0200

    update1

--- src/MeniuPrincipal1.java
+++ src/MeniuPrincipal1.java
@@ -4,10 +4,15 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import java.awt.Font;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTabbedPane;
+import javax.swing.JButton;
 
 public class MeniuPrincipal1 {
 
 	private JFrame frame;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -24,7 +29,7 @@ public class MeniuPrincipal1 {
 			}
 		});
 	}
-	
+
 	/**
 	 * Create the application.
 	 */
@@ -37,16 +42,25 @@ public class MeniuPrincipal1 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 710, 489);
+		frame.setBounds(100, 100, 904, 754);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblMeniu = new JLabel("Meniu");
-		lblMeniu.setFont(new Font("Times New Roman", Font.PLAIN, 43));
-		lblMeniu.setBounds(273, 13, 196, 111);
-		frame.getContentPane().add(lblMeniu);
+		JLabel lblMeniuOperatiiFinanciar = new JLabel("Meniu operatii financiar contabile");
+		lblMeniuOperatiiFinanciar.setFont(new Font("Times New Roman", Font.PLAIN, 43));
+		lblMeniuOperatiiFinanciar.setBounds(146, 35, 584, 56);
+		frame.getContentPane().add(lblMeniuOperatiiFinanciar);
+		
+		JButton btnProfitPoliclinici = new JButton("Profit policlinici");
+		btnProfitPoliclinici.setBounds(419, 150, 101, 23);
+		frame.getContentPane().add(btnProfitPoliclinici);
+		
+		JButton btnNewButton = new JButton("Profit medici");
+		btnNewButton.setBounds(641, 150, 101, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		table = new JTable();
+		table.setBounds(287, 217, 591, 385);
+		frame.getContentPane().add(table);
 	}
-
-	
-
 }
